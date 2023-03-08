1725c1eb9c4e43d704d5ddd9503a04fb45be615f
Léo Bufi <bufi.leo@gmail.com>
2023-03-08 12:13:31 +0100

    adding and suppressing tags to project OK

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ApplicationRecord
+    has_many :joints, dependent: :destroy
+    has_many :projects, through: :joints
+
+    validates :name, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_07_085114) do
+ActiveRecord::Schema.define(version: 2023_03_07_162709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "joints", force: :cascade do |t|
+    t.bigint "project_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_joints_on_project_id"
+    t.index ["tag_id"], name: "index_joints_on_tag_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -42,4 +51,6 @@ ActiveRecord::Schema.define(version: 2023_03_07_085114) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "joints", "projects"
+  add_foreign_key "joints", "tags"
 end
